a493a263a41c3f39fd5fa0d7feb7929aaa70a671
Shivani Jetty <shivanijetty@gmail.com>
2022-10-06 21:44:52 -0400

    commit tree 0b107ef057cb1d9bfff637c5b8cd67ed18d5d7b2

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const element = document.getElementById("main")
+element.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Shivani is the champion";
